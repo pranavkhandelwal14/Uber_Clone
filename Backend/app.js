9fcf34b2d7a7b217101417495a92e069f48e02fc
Pranav Khandelwal <pranav.khandelwal@infoneotech.com>
2025-12-17 14:43:06 +0530

    create captain user code

--- Backend/app.js
+++ Backend/app.js
@@ -4,8 +4,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connectToDb = require('./db/db');
-const userRoutes = require('./routes/user.routes');
 const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
 
 
 connectToDb();
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
-app.use('/users', userRoutes)
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 module.exports = app;
