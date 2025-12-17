236f4905c9382344ce28635794574ddb7007ae79
Pranav Khandelwal <pranav.khandelwal@infoneotech.com>
2025-12-17 12:53:46 +0530

    commit authorization of user part

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   fullname: {
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     require: true,
     unique: true,
-    minlength: [5, 'Email must be at least 5 characters long'],
+    minlength: [5, "Email must be at least 5 characters long"],
   },
   password: {
     type: String,
@@ -27,22 +27,24 @@ const userSchema = new mongoose.Schema({
   },
   socketId: {
     type: String,
-  }
-})
+  },
+});
 
-userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({ _id: this.id }, process.env.JWT_SECRET);
-    return token;
-}
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+  return token;
+};
 
 userSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-}
+  return await bcrypt.compare(password, this.password);
+};
 
 userSchema.statics.hashPassword = async function (password) {
-    return await bcrypt.hash(password, 10);
-}
+  return await bcrypt.hash(password, 10);
+};
 
-const userModel = mongoose.model('user', userSchema);
+const userModel = mongoose.model("user", userSchema);
 
 module.exports = userModel;
